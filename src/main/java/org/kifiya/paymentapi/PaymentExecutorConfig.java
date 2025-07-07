f3b3b9e146554b6f430e7f46e06132dc50bd101e
betsegawlemma <betsegawlemma@gmail.com>
2025-07-07 11:34:29 +0300

    add PaymentExecutorConfig

--- src/main/java/org/kifiya/paymentapi/PaymentExecutorConfig.java
+++ src/main/java/org/kifiya/paymentapi/PaymentExecutorConfig.java
@@ -1,4 +1,21 @@
 package org.kifiya.paymentapi;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
 public class PaymentExecutorConfig {
+
+    @Bean(name = "paymentTaskExecutor")
+    public TaskExecutor paymentTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5); // adjust based on load profile
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("payment-task-");
+        executor.initialize();
+        return executor;
+    }
 }
